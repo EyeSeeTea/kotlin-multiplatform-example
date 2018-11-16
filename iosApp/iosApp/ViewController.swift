1a9b8874c5335606d3a3dc0751862cf1cbfa0bf0
Jorge Sánchez <xurxosf@gmail.com>
2018-11-16 17:10:33 +0100

    Create iosApp

---
+++ iosApp/iosApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  iosApp
+//
+//  Created by Jorge Sánchez on 16/11/18.
+//  Copyright © 2018 xurxodev. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
